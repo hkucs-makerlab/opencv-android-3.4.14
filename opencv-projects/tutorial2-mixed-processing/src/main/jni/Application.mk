c6a63e7002c4fc6dc236bb5811008c9b54a27eed
hkucs-makerlab <makerlab@cs.hku.hk>
2021-05-27 16:01:40 +0800

    update

--- opencv-projects/tutorial2-mixed-processing/src/main/jni/Application.mk
+++ opencv-projects/tutorial2-mixed-processing/src/main/jni/Application.mk
@@ -1,4 +1,6 @@
-APP_STL := gnustl_static
+#https://developer.android.com/ndk/guides/cpp-support
+APP_STL := c++_shared
+#APP_STL := c++_static
 APP_CPPFLAGS := -frtti -fexceptions
-APP_ABI := armeabi-v7a
-APP_PLATFORM := android-8
+APP_ABI :=  armeabi-v7a arm64-v8a arm64-v8a x86
+APP_PLATFORM := android-16
